74623f3d88e66eac69da0c6cfcb73eb699566a8d
5Grains <shtdo0307@gmail.coms>
2024-10-04 00:59:09 +0900

    modified1

--- calculate.py
+++ calculate.py
@@ -1,5 +1,5 @@
-first_number = int(input("First Number: "))
-second_number = int(input("Second Number: "))
+first_number = int(input("First Number: 1"))
+second_number = int(input("Second Number: 1"))
 
 sum = first_number + second_number
 difference = first_number + second_number
