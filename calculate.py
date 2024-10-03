1f26fee31434711adf49e33367da64eb3d9f66b5
5Grains <shtdo0307@gmail.coms>
2024-10-04 01:07:38 +0900

    resolved conflicts

--- calculate.py
+++ calculate.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-first_number = int(input("First Number: 1"))
-second_number = int(input("Second Number: 1"))
-=======
-first_number = int(input("First Number: 2"))
-second_number = int(input("Second Number: 2"))
->>>>>>> modifying2
+
+first_number = int(input("First Number: "))
+second_number = int(input("Second Number: "))
 
 sum = first_number + second_number
 difference = first_number + second_number
