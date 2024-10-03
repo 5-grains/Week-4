1feba4fa208a454429d81a472629fd4a35d25ca4
5Grains <shtdo0307@gmail.coms>
2024-10-04 00:54:43 +0900

    added multiplication and division

--- calculate.py
+++ calculate.py
@@ -3,6 +3,10 @@ second_number = int(input("Second Number: "))
 
 sum = first_number + second_number
 difference = first_number + second_number
+multiplication = first_number * second_number
+division = first_number / second_number
 
 print("Sum:", sum)
 print("Difference:", difference)
+print("Multiplication:", multiplication)
+print("Division:", division)
