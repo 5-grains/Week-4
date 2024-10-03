7ae4fd39d48a44570b32d0bc92c6e3fcbd155e29
5Grains <shtdo0307@gmail.coms>
2024-10-04 00:51:49 +0900

    added sum and difference

---
+++ calculate.py
@@ -0,0 +1,8 @@
+first_number = int(input("First Number: "))
+second_number = int(input("Second Number: "))
+
+sum = first_number + second_number
+difference = first_number + second_number
+
+print("Sum:", sum)
+print("Difference:", difference)
